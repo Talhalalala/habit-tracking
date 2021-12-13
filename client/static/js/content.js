e50e2371c376b2d93563dad5a3646450674bd21e
EmilyKral <emilykral@gmail.com>
2021-12-13 17:20:10 +0000

    added create habit form and request

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -1,7 +1,7 @@
-const { requestLogin, requestRegistration, currentUser } = require('./auth')
-const { getHabits, getInfoAboutHabit, updateHabit } = require("./requests")
+const { requestLogin, requestRegistration, currentUser } = require("./auth");
+const { getHabits, getInfoAboutHabit, updateHabit, addHabit } = require("./requests");
 
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 
 // function renderHomepage(){
 //     const title = document.createElement('h2')
@@ -9,90 +9,125 @@ const main = document.querySelector('main');
 //     main.appendChild(title)
 // }
 
-
 function renderLoginForm() {
-    const fields = [
-        { tag: 'label', textContent:'Email', attributes: { for: 'email' }},
-        { tag: 'input', attributes: { type: 'email', name: 'email' } },
-        { tag: 'label', textContent:'Password', attributes: { for: 'password' }},
-        { tag: 'input', attributes: { type: 'password', name: 'password' } },
-        { tag: 'input', attributes: { type: 'submit', value: 'Login' } }
-    ]
-    const form = document.createElement('form');
-    fields.forEach(f => {
-        let field = document.createElement(f.tag);
-        if (f.textContent) { field.textContent = f.textContent }
-        Object.entries(f.attributes).forEach(([a, v]) => {
-            field.setAttribute(a, v);
-            form.appendChild(field);
-        })
-    })
-    form.addEventListener('submit', requestLogin)
-    main.appendChild(form);
+	const fields = [
+		{ tag: "label", textContent: "Email", attributes: { for: "email" } },
+		{ tag: "input", attributes: { type: "email", name: "email" } },
+		{ tag: "label", textContent: "Password", attributes: { for: "password" } },
+		{ tag: "input", attributes: { type: "password", name: "password" } },
+		{ tag: "input", attributes: { type: "submit", value: "Login" } }
+	];
+	const form = document.createElement("form");
+	fields.forEach(f => {
+		let field = document.createElement(f.tag);
+		if (f.textContent) {
+			field.textContent = f.textContent;
+		}
+		Object.entries(f.attributes).forEach(([a, v]) => {
+			field.setAttribute(a, v);
+			form.appendChild(field);
+		});
+	});
+	form.addEventListener("submit", requestLogin);
+	main.appendChild(form);
 }
 
 function renderRegisterForm() {
-    const fields = [
-        { tag: 'label', textContent:'Username', attributes: { for: 'username' }},
-        { tag: 'input', attributes: { type: 'text', name: 'username' } },
-        { tag: 'label', textContent:'Email', attributes: { for: 'email' }},
-        { tag: 'input', attributes: { type: 'email', name: 'email' } },
-        { tag: 'label', textContent:'Password', attributes: { for: 'password' }},
-        { tag: 'input', attributes: { type: 'password', name: 'password' } },
-        { tag: 'label', textContent:'Confirm Password', attributes: { for: 'passwordConfirmation' }},
-        { tag: 'input', attributes: { type: 'password', name: 'passwordConfirmation' } },
-        { tag: 'input', attributes: { type: 'submit', value: 'Create Account' } }
-    ]
-    const form = document.createElement('form');
-    fields.forEach(f => {
-        let field = document.createElement(f.tag);
-        if (f.textContent) { field.textContent = f.textContent }
-        Object.entries(f.attributes).forEach(([a, v]) => {
-            field.setAttribute(a, v);
-            form.appendChild(field);
-        })
-    })
-    form.addEventListener('submit', requestRegistration)
-    main.appendChild(form);
+	const fields = [
+		{ tag: "label", textContent: "Username", attributes: { for: "username" } },
+		{ tag: "input", attributes: { type: "text", name: "username" } },
+		{ tag: "label", textContent: "Email", attributes: { for: "email" } },
+		{ tag: "input", attributes: { type: "email", name: "email" } },
+		{ tag: "label", textContent: "Password", attributes: { for: "password" } },
+		{ tag: "input", attributes: { type: "password", name: "password" } },
+		{ tag: "label", textContent: "Confirm Password", attributes: { for: "passwordConfirmation" } },
+		{ tag: "input", attributes: { type: "password", name: "passwordConfirmation" } },
+		{ tag: "input", attributes: { type: "submit", value: "Create Account" } }
+	];
+	const form = document.createElement("form");
+	fields.forEach(f => {
+		let field = document.createElement(f.tag);
+		if (f.textContent) {
+			field.textContent = f.textContent;
+		}
+		Object.entries(f.attributes).forEach(([a, v]) => {
+			field.setAttribute(a, v);
+			form.appendChild(field);
+		});
+	});
+	form.addEventListener("submit", requestRegistration);
+	main.appendChild(form);
 }
 
-async function RenderToday() {
-    let data =  await getHabits(currentUser())
-    const feed = document.createElement('section');
-    feed.id = 'feed';
-    if(data.err){return}
-    
-    data.forEach(renderHabits);
-    main.appendChild(feed);
+async function renderToday() {
+	let data = await getHabits(currentUser());
+	const feed = document.createElement("section");
+	feed.id = "feed";
+	if (data.err) {
+		return;
+	}
+
+	data.forEach(renderHabits);
+	main.appendChild(feed);
 }
 
 const renderHabits = habitData => {
-    const post = document.createElement('div');
-    post.className = 'post';
-    const habit = document.createElement('h3');
-    const frequency = document.createElement('p');
-    habit.textContent = habitData.habitName;
-    frequency.textContent = `Every ${habitData.frequency} days`;
-    const fields = [
-        { tag: 'label', textContent:`Amount (${habitData.frequency})`, attributes: { for: 'amount' }},
-        { tag: 'input', attributes: { type: 'text', name: 'amount' } },
-        { tag: 'input', attributes: { type: 'submit', value: 'Log Data' } }
-    ]
-    const form = document.createElement('form');
-    form.id = habitData.habit_ID
-    fields.forEach(f => {
-        let field = document.createElement(f.tag);
-        if (f.textContent) { field.textContent = f.textContent }
-        Object.entries(f.attributes).forEach(([a, v]) => {
-            field.setAttribute(a, v);
-            form.appendChild(field);
-        })
-    })
-    form.addEventListener('submit', updateHabit)
-    main.appendChild(form);
-    post.appendChild(user);
-    post.appendChild(body);
-    feed.appendChild(post);
+	const post = document.createElement("div");
+	post.className = "post";
+	const habit = document.createElement("h3");
+	const frequency = document.createElement("p");
+	habit.textContent = habitData.habitName;
+	frequency.textContent = `Every ${habitData.frequency} days`;
+	const fields = [
+		{ tag: "label", textContent: `Amount (${habitData.frequency})`, attributes: { for: "amount" } },
+		{ tag: "input", attributes: { type: "text", name: "amount" } },
+		{ tag: "input", attributes: { type: "submit", value: "Log Data" } }
+	];
+	const form = document.createElement("form");
+	form.id = habitData.habit_ID;
+	fields.forEach(f => {
+		let field = document.createElement(f.tag);
+		if (f.textContent) {
+			field.textContent = f.textContent;
+		}
+		Object.entries(f.attributes).forEach(([a, v]) => {
+			field.setAttribute(a, v);
+			form.appendChild(field);
+		});
+	});
+	form.addEventListener("submit", updateHabit);
+	main.appendChild(form);
+	post.appendChild(user);
+	post.appendChild(body);
+	feed.appendChild(post);
+};
+
+function renderNewHabit() {
+	const fields = [
+		{ tag: "label", textContent: "Habit Name", attributes: { for: "habit" } },
+		{ tag: "input", attributes: { type: "text", name: "habit" } },
+		{ tag: "label", textContent: "Frequency", attributes: { for: "frequency" } },
+		{ tag: "input", attributes: { type: "text", name: "frequency" } },
+		{ tag: "label", textContent: "Goal", attributes: { for: "goal" } },
+		{ tag: "input", attributes: { type: "text", name: "goal" } },
+		{ tag: "label", textContent: "Units", attributes: { for: "unit" } },
+		{ tag: "input", attributes: { type: "text", name: "unit" } },
+		{ tag: "input", attributes: { type: "submit", value: "Login" } }
+	];
+
+	const form = document.createElement("form");
+	fields.forEach(f => {
+		let field = document.createElement(f.tag);
+		if (f.textContent) {
+			field.textContent = f.textContent;
+		}
+		Object.entries(f.attributes).forEach(([a, v]) => {
+			field.setAttribute(a, v); //attribute-value
+			form.appendChild(field);
+		});
+	});
+	form.addEventListener("submit", addHabit);
+	main.appendChild(form);
 }
 
-module.exports = { renderLoginForm, renderRegisterForm }
+module.exports = { renderLoginForm, renderRegisterForm, renderHabits, renderToday, renderNewHabit };
--- db/_seeds.sql
+++ db/_seeds.sql
@@ -2,13 +2,13 @@ INSERT INTO users (username, email, hpassword)
 Values
     ('example', 'exampleemail@email.com', 'examplepassword');
 
-INSERT INTO habits (habit_ID, user_ID, habit, frequency, goal, units, streak, long_streak)
+INSERT INTO habits (habit_ID, user_ID, habit, frequency, goal, units, streak)
 Values
-    (1, 1, 'Steps', 1, 10000, 'Day', 3, 8000);
+    (1, 1, 'Steps', 1, 10000, 'Day', 3);
 
 INSERT INTO habit_data (habit_data_id, habit_ID, interval_start, interval_end, habit_amount, habit_achieved)
 Values
-    (1, 1, '2021-12-12', '2021-12-13', 5000, 0);
+    (1, 1, '2021-12-12', '2021-12-13', 5000, false);
 
 
 
