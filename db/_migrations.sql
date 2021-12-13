da5b4fec84f616471bd32d92a6f2c597ea82904c
Rahib <rrahman829@outlook.com>
2021-12-13 22:12:38 +0000

    changed some sql table values

--- db/_migrations.sql
+++ db/_migrations.sql
@@ -11,19 +11,20 @@ DROP TABLE IF EXISTS habits;
 
 CREATE TABLE habits (
     habit_ID serial PRIMARY KEY, 
-    user_ID INT,
+    user_ID INT FOREIGN KEY REFERENCES users(user_ID) ON DELETE CASCADE,
     habit VARCHAR(100),
     frequency INT, 
     goal INT, 
     units VARCHAR(50),
-    streak INT
+    streak INT DEFAULT 0,
+    creation_date date DEFAULT CAST(GETDATE() AS Date)
 );
 
 DROP TABLE IF EXISTS habit_data;
 
 CREATE TABLE habit_data (
     habit_data_id serial PRIMARY KEY, 
-    habit_ID INT,
+    habit_ID INT FOREIGN KEY REFERENCES habits(habit_ID) ON DELETE CASCADE,
     interval_start DATE,
     interval_end DATE,
     habit_amount INT, 
