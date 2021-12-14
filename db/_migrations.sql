151bdffdc16f7a1206c5355d023fb3eefb3046a3
Rahib <rrahman829@outlook.com>
2021-12-14 12:42:32 +0000

    small sql changes

--- db/_migrations.sql
+++ db/_migrations.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    user_ID INT, 
+    user_ID INT PRIMARY KEY, 
     username VARCHAR(20) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     hpassword VARCHAR(25) NOT NULL
@@ -11,20 +11,19 @@ DROP TABLE IF EXISTS habits;
 
 CREATE TABLE habits (
     habit_ID serial PRIMARY KEY, 
-    user_ID INT FOREIGN KEY REFERENCES users(user_ID) ON DELETE CASCADE,
+    user_ID INT, 
     habit VARCHAR(100),
     frequency INT, 
     goal INT, 
     units VARCHAR(50),
-    streak INT DEFAULT 0,
-    creation_date date DEFAULT CAST(GETDATE() AS Date)
+    streak INT DEFAULT 0
 );
 
 DROP TABLE IF EXISTS habit_data;
 
 CREATE TABLE habit_data (
     habit_data_id serial PRIMARY KEY, 
-    habit_ID INT FOREIGN KEY REFERENCES habits(habit_ID) ON DELETE CASCADE,
+    habit_ID INT, 
     interval_start DATE,
     interval_end DATE,
     habit_amount INT, 
