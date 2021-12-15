de544420dffd91313a251d798a4d562fb2127f61
rjasmine29 <jasmine.r@hotmail.com>
2021-12-15 11:04:58 +0000

    css updated

--- client/static/testing/auth.spec.js
+++ client/static/testing/auth.spec.js
@@ -5,8 +5,7 @@
  const fs = require("fs");
  const path = require("path");
  const { expect } = require("@jest/globals");
-const { requestRegistration } = require("../js/auth.js");
- const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
+ const html = fs.readFileSync(path.resolve(__dirname, "../../index.html"), "utf8");
  
  global.fetch = require("jest-fetch-mock");
  let app;
@@ -21,11 +20,25 @@ const { requestRegistration } = require("../js/auth.js");
          fetch.resetMocks();
      });
 
-    test('requestsLogin calls fetch', async ()=>{
+    /*test('requestsLogin calls fetch', async ()=>{
         await requests.requestLogin({});
         expect(fetch).toHaveBeenCalled();
-    })
-    
+    }) */
+    describe("requestLogin", () => {
+		it("makes a fetch request", () => {
+			app.requestLogin({});
+			expect(fetch).toHaveBeenCalled();
+		});
+	
+
+    describe("requestRegistration", () => {
+		it("makes a fetch request", () => {
+			app.requestRegistration({});
+			expect(fetch).toHaveBeenCalled();
+		});
+	}); 
+
+    });
     
 
 })
